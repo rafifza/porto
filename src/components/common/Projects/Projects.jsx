1ffd7794ada53777123f4912c1ce6bfe6c8ff4e3
rafifza <115349069+rafifza@users.noreply.github.com>
2022-10-09 22:44:04 +0700

    Update Projects.jsx

--- src/components/common/Projects/Projects.jsx
+++ src/components/common/Projects/Projects.jsx
@@ -13,12 +13,14 @@ function Project () {
             </div>
             <div className="ss1">
                 <img src="./src/assets/ss1.png" height={300}></img>
+                <img src="/src/assets/ss1.png" height={300}></img>
                 <div className="code1">
                     <a href="https://github.com/rafifza/Project-LAFORANT">Source code </a>
                     
                 </div>
             </div>
             <div className="ss2">
+                <img src="/src/assets/ss2.png" height={300}></img>
                 <div className="code2">
                     <a href="https://github.com/rafifza/projectentre">Source code </a>
                  </div>
