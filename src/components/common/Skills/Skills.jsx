1d4972c89d0dc2c38207e2bbbab378b3a665befc
rafifza <115349069+rafifza@users.noreply.github.com>
2024-10-17 10:42:26 +0700

    update skills 17/10/2024

--- src/components/common/Skills/Skills.jsx
+++ src/components/common/Skills/Skills.jsx
@@ -9,84 +9,34 @@ function Skills() {
       </div>
       <div className="conta">
         <div className="css">
-          <img
-            src="../../assets/css.png"
-            alt="css"
-            height={100}
-            width={70}
-          ></img>
+          <img src="../../assets/css.png" alt="css" />
         </div>
         <div className="html">
-          <img
-            src="../../assets/html.png"
-            alt="html"
-            height={100}
-            width={100}
-          ></img>
-        </div>{" "}
+          <img src="../../assets/html.png" alt="html" />
+        </div>
         <div className="js">
-          <img
-            src="../../assets/js.png"
-            alt="js"
-            height={100}
-            width={100}
-          ></img>
+          <img src="../../assets/js.png" alt="js" />
         </div>
         <div className="figma">
-          <img
-            src="../../assets/figma.svg"
-            alt="figma"
-            height={90}
-            width={90}
-          ></img>
+          <img src="../../assets/figma.svg" alt="figma" />
         </div>
         <div className="vsc">
-          <img
-            src="../../assets/vsc.png"
-            alt="vsc"
-            height={90}
-            width={90}
-          ></img>
+          <img src="../../assets/vsc.png" alt="vsc" />
         </div>
         <div className="react">
-          <img
-            src="../../assets/react.png"
-            alt="vsc"
-            height={100}
-            width={100}
-          ></img>
-        </div>{" "}
+          <img src="../../assets/react.png" alt="react" />
+        </div>
         <div className="react">
-          <img
-            src="../../assets/reactnative.png"
-            alt="vsc"
-            height={90}
-            width={90}
-          ></img>
+          <img src="../../assets/reactnative.png" alt="react native" />
         </div>
         <div className="node">
-          <img
-            src="../../assets/node.png"
-            alt="vsc"
-            height={90}
-            width={90}
-          ></img>
-        </div>{" "}
+          <img src="../../assets/node.png" alt="node" />
+        </div>
         <div className="dart">
-          <img
-            src="../../assets/dart.png"
-            alt="vsc"
-            height={90}
-            width={110}
-          ></img>
+          <img src="../../assets/dart.png" alt="dart" />
         </div>
         <div className="flutter">
-          <img
-            src="../../assets/flutter.png"
-            alt="vsc"
-            height={80}
-            width={100}
-          ></img>
+          <img src="../../assets/flutter.png" alt="flutter" />
         </div>
       </div>
     </section>
